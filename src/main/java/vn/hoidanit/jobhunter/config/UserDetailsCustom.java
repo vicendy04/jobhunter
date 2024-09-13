76610ffde6787736249cd16c4c12c4a9bed19c6a
vicendy04 <htvd1204@gmail.com>
2024-09-13 18:40:51 +0700

    refactor service

--- src/main/java/vn/hoidanit/jobhunter/config/UserDetailsCustom.java
+++ src/main/java/vn/hoidanit/jobhunter/config/UserDetailsCustom.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.jobhunter.service;
+package vn.hoidanit.jobhunter.config;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import vn.hoidanit.jobhunter.service.UserService;
 
 import java.util.Collections;
 
